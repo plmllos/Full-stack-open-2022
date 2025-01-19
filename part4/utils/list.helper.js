c6fd2eefe5d38c50b0ebf6d1d080310b56f41a39
Milos Popovic <popovic.milos@live.com>
2025-01-19 17:58:23 +0100

    part4 exercises 4.3 - 4.5

---
+++ part4/utils/list.helper.js
@@ -0,0 +1,24 @@
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  const favorite = blogs.reduce((prev, current) =>
+    prev.likes > current.likes ? prev : current
+  );
+  return {
+    title: favorite.title,
+    author: favorite.author,
+    likes: favorite.likes,
+  };
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+};
